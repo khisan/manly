9bb9bf10bd6352021bf072217a52f39beb8f9d75
khisan <khisan8@gmail.com>
2020-03-09 19:24:36 +0700

    rubah next page pages index to more

--- application/views/Frontend/index.php
+++ application/views/Frontend/index.php
@@ -256,8 +256,11 @@
         <?php endforeach; ?>
       </div>
 
-      <!-- <div class="pagging"> -->
-      <?php echo $pagination; ?>
-      <!-- </div> -->
+      <div class="flex-c-m flex-w w-full p-t-45">
+        <a href="<?php echo site_url('Shop');?>" class="flex-c-m stext-101 cl5 size-103 bg2 bor1 hov-btn1 p-lr-15 trans-04">
+            More
+        </a>
+      </div>
+
     </div>
   </section>
