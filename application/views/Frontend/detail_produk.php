a69fe5e5e6b8247aac4ff6c5304a938df89c5637
khisan <khisan8@gmail.com>
2020-03-09 19:17:18 +0700

    rubah breadcumb name shop to product some pages frontend

--- application/views/Frontend/detail_produk.php
+++ application/views/Frontend/detail_produk.php
@@ -130,7 +130,7 @@
       </a>
 
       <a href="<?php echo site_url('shop');?>" class="stext-109 cl8 hov-cl1 trans-04">
-        Shop
+        Product
         <i class="fa fa-angle-right m-l-9 m-r-10" aria-hidden="true"></i>
       </a>
       <?php foreach ($data->result() as $row):?>
