0c2dcd92195756467951eff590a67e8da9b50aa0
khisan <khisan8@gmail.com>
2020-03-10 14:36:53 +0700

    setting text-align about pages frontend

--- application/views/Frontend/about.php
+++ application/views/Frontend/about.php
@@ -153,11 +153,11 @@
     <div class="row">
       <div class="order-md-2 col-md-7 col-lg-8 p-b-30">
         <div class="p-t-7 p-l-85 p-l-15-lg p-l-0-md">
-          <h3 class="mtext-111 cl2 p-b-16">
+          <h3 class="mtext-111 cl2 p-b-16" style="text-align: center;">
             <?php echo $judul_deskripsi_2; ?>
           </h3>
 
-          <p class="stext-113 cl6 p-b-26">
+          <p class="stext-113 cl6 p-b-26" style="text-align: justify;">
             <?php echo $isi_deskripsi_2; ?>
           </p>
         </div>
