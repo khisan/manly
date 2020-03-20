c677f0a7083e8e55ec6f7e5994ecee7616f4c86f
khisan <khisan8@gmail.com>
2020-03-20 14:07:06 +0700

    fix gambar header_blog

--- application/views/Frontend/detail_blog.php
+++ application/views/Frontend/detail_blog.php
@@ -121,7 +121,7 @@
   </header>
 
 <!-- Title page -->
-<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('<?php echo base_url('assets/images/bg-02.jpg')?>');">
+<section class="bg-img1 txt-center p-lr-15 p-tb-92" style="background-image: url('<?php echo base_url('assets/images/header_blog/bg-02.jpg')?>');">
   <h2 class="ltext-105 cl0 txt-center">
     Blog
   </h2>
